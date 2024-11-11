3cb8aa0d244cf99ab9ba47dd7fdc6dea5b3a5153
Petrik Diak <diak@petrik.hu>
2024-11-11 14:25:08 +0100

    Prisma

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -18,8 +18,8 @@ export class BooksController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    const book = this.booksService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    const book = await this.booksService.findOne(+id);
     if (!book) throw new NotFoundException('No book with ID ' + id);
 
     return book;
@@ -35,8 +35,8 @@ export class BooksController {
 
   @Delete(':id')
   @HttpCode(204)
-  remove(@Param('id') id: string) {
-    const success = this.booksService.remove(+id);
+  async remove(@Param('id') id: string) {
+    const success = await this.booksService.remove(+id);
     if (!success) {
       throw new NotFoundException('No book with ID ' + id);
     }
